89282961685bb5d23909cee7ae8a8093c2e00b88
Paul Boursin <paul.boursin@gmail.com>
2022-03-06 21:40:03 +0100

    dijktra algo

--- miniprojet/src/algorithms/DefaultTeam.java
+++ miniprojet/src/algorithms/DefaultTeam.java
@@ -2,6 +2,10 @@ package algorithms;
 
 import java.awt.Point;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.PriorityQueue;
+import algorithms.Tree2D;
 
 import supportGUI.*;
 
@@ -266,12 +270,168 @@ public class DefaultTeam {
 		}
 	}
 	
+	public void addEdge(HashMap<Integer, ArrayList<Integer>> edges, int i, int j) {
+		var list = edges.get(i);
+		if(list == null) {
+			list = new ArrayList<Integer>();
+			edges.put(i, list);
+		}
+		list.add(j);
+	}
 	
 	
+	public HashMap<Integer, ArrayList<Integer>> createGeometricGraph(ArrayList<Point> points, int edgeThreshold) {
+		var edges = new HashMap<Integer,ArrayList<Integer>>();
+		
+		for(int i = 0; i < points.size(); i++) {
+			for(int j = 0; j < points.size(); j++) {
+				if (points.get(i).distance(points.get(j)) < edgeThreshold) {
+					addEdge(edges, i, j);
+					addEdge(edges, j, i);
+				}
+			}
+		}
+		
+		return edges;
+	}
 	
+	class DijkstraData {
+		public DijkstraData(int point, double distance) {
+			this.point = point;
+			this.distance = distance;
+		}
+		int point; // le point actuel pour la priority queue, le point précédent dans la hashmap
+		double distance; // distance au point de départ
+	}
+	
+	
+	public double shortestPathToTree(ArrayList<Point> points, HashMap<Integer, ArrayList<Integer>> edges, HashSet<Integer> treePoints, int start, ArrayList<Integer> path) {
+		
+		var pq = new PriorityQueue<DijkstraData>((a, b) -> {return a.distance < b.distance ? -1 : 1;});
+		var map = new HashMap<Integer, DijkstraData>();
+		
+		pq.add(new DijkstraData(start, 0));
+		map.put(start, new DijkstraData(-1, 0));
+		
+		while(!pq.isEmpty()) {
+			var u = pq.poll();
+			
+			if(treePoints.contains(u.point)) {
+				int p = u.point;
+				while(p != -1) {
+					path.add(p);
+					p = map.get(p).point;
+				}
+				return u.distance;
+			}
+			
+			var adjacents = edges.get(u.point);
+			for(int adjacent : adjacents) {
+				double distance = u.distance + points.get(u.point).distance(points.get(adjacent));
+				
+				var v = map.get(adjacent);
+				if(v == null) {
+					v = new DijkstraData(u.point, distance);
+					map.put(adjacent, v);
+				} else if(v.distance > distance) {
+					v.distance = distance;
+				} else {
+					continue;
+				}
+				
+				pq.add(new DijkstraData(adjacent, v.distance));
+				
+			}
+			
+		}
+		
+		return -1;
+		
+	}
+	
+	public Tree2D findPointInTree(Tree2D tree, Point point) {
+		
+		if(tree.getRoot().equals(point)) {
+			return tree;
+		}
+		
+		for(Tree2D subTree : tree.getSubTrees()) {
+			var ret = findPointInTree(subTree, point);
+			if(ret != null) return ret;
+		}
+		
+		return null;
+		
+	}
+	
+	public Tree2D addPathToTree(Tree2D tree, ArrayList<Point> points, ArrayList<Integer> path) {
+		var pathTree = findPointInTree(tree, points.get(path.get(0)));
+		
+		for(int i = 1; i<path.size(); i++) {
+			int p = path.get(i);
+			var ntree = new Tree2D(points.get(p), new ArrayList<>());
+			pathTree.getSubTrees().add(ntree);
+			pathTree = ntree;
+		}
+		
+		return tree;
+		
+	}
+	
+	public int findPoint(ArrayList<Point> points, Point point) {
+		for(int i = 0; i<points.size(); i++) {
+			var p = points.get(i);
+			if(p.equals(point)) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	
+	public Tree2D calculSteinerDijktra(ArrayList<Point> points, int edgeThreshold, ArrayList<Point> hitPoints) {
+		var edges = createGeometricGraph(points, edgeThreshold);
+		
+		var treePoints = new HashSet<Integer>();
+		
+		var start = hitPoints.get(0);
+		int startIndex = findPoint(points, start);
+		
+		Tree2D tree = new Tree2D(start, new ArrayList<>());
+		treePoints.add(startIndex);
+		
+		for(int h = 1; h<hitPoints.size(); h++) {
+			double minDistance = 0;
+			ArrayList<Integer> bestPath = new ArrayList<>();
+			for(int i = 0; i<hitPoints.size(); i++) {
+				var hitPoint = findPoint(points, hitPoints.get(i));
+				if(treePoints.contains(hitPoint)) continue;
+				var path = new ArrayList<Integer>();
+				double distance = shortestPathToTree(points, edges, treePoints, hitPoint, path);
+				//System.out.println("point " + i + " distance " + distance + " pathsize " + path.size());
+				if((minDistance == 0.0 || distance < minDistance) && (distance != -1 && path.size() > 1)) {
+					minDistance = distance;
+					bestPath = path;
+				}
+			}
+			
+			if(minDistance == 0.0) return tree;
+			
+			//System.out.println("adding point " + (h+1) + " distance " + minDistance + " pathsize " + bestPath.size());
+			
+			tree = addPathToTree(tree, points, bestPath);
+
+			for(int p : bestPath) {
+				treePoints.add(p);
+			}
+		}
+		
+		return tree;
+	}
 	
 	
 
+	/*
 
 	public Tree2D calculSteiner(ArrayList<Point> points, int edgeThreshold, ArrayList<Point> hitPoints) {
 		
@@ -306,6 +466,11 @@ public class DefaultTeam {
 		LineToTree(aretes);
 		
 		return this.Tree;
+	}
+	*/
+	
+	public Tree2D calculSteiner(ArrayList<Point> points, int edgeThreshold, ArrayList<Point> hitPoints) {
+		return calculSteinerDijktra(points, edgeThreshold, hitPoints);
 	}
 
 	public Tree2D calculSteinerBudget(ArrayList<Point> points, int edgeThreshold, ArrayList<Point> hitPoints) {
