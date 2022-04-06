918f451a0a9f1087bd7603b3c8957ceb5456eb9d
Quentin-Palmisano <palmisanoquentin@gmail.com>
2022-04-06 23:22:12 +0200

    deplacement

--- projet/js/index.js
+++ projet/js/index.js
@@ -1,3 +1,15 @@
+let map;
+let height=11;
+let width=13;
+let P1x=1;
+let P1y=1;
+let P2x=height-2;
+let P2y=width-2;
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
 function drawImage(nom, x, y) {
     var c = document.getElementById("myCanvas");
     var ctx = c.getContext("2d");
@@ -12,53 +24,115 @@ ctx.font = "30px Arial";
 ctx.fillText(text, x, y);
 }
 
-function randomMap(height, width){
-    var map = "";
-    var result = new Array(height);
+function randomMap(x){
+    map = new Array(height);
 
     for(var i=0; i<height; i++){
-        result[i] = new Array(width);
+        map[i] = new Array(width);
         for(var j=0; j<width; j++){
             if(j==0 || j==width-1 || i==0 || i==height-1){
-                result[i][j] = 'X';
+                map[i][j] = 'X';
             } else if((i==1 && (j==1||j==2||j==width-3||j==width-2))
             ||(i==2 && (j==1||j==width-2))
             ||(i==height-2 && (j==1||j==2||j==width-3||j==width-2))
             ||(i==height-3 && (j==1||j==width-2))){
-                result[i][j] = ' ';
+                map[i][j] = ' ';
 
             }else{
                 if(i%2==0 && j%2==0){ 
-                    result[i][j] = 'X';
+                    map[i][j] = 'X';
                 } else {
                     var rand = Math.floor(Math.random() * 100);
-                    if(rand<60){
-                        result[i][j] = 'O';
+                    if(rand<x){
+                        map[i][j] = 'O';
                     }else{
-                        result[i][j] = ' ';
+                        map[i][j] = ' ';
                     }
                 }   
             }
         }
+        
     }
-    return result;
+    map[P1x][P1y]='P';
+    map[P2x][P2y]='Q';
+    return map;
 }
-function drawMap(tab){
-    for(var i= 0; i < tab.length; i++) {
-        for(var j= 0; j < tab[i].length; j++) {
-            if(tab[i][j]=='X'){
+
+async function drawPlayer(map){
+    var i = 0;
+    while(true){
+        drawMap(map);
+        i++;
+        drawImage("player1_"+((i%14)+1), 43, 38);
+        await sleep(300);
+    }
+}
+
+async function drawMap(){
+    for(var i= 0; i < map.length; i++) {
+        for(var j= 0; j < map[i].length; j++) {
+            if(map[i][j]=='X'){
                drawImage("block_incassable", i*43, j*38);
-            } else if(tab[i][j]=='O'){
+            } else if(map[i][j]=='O'){
                 drawImage("block_cassable", i*43, j*38);
-            } else if(tab[i][j]==' '){
-                drawImage("vide", i*43, j*38);
-                }
+            } else if(map[i][j]==' '){
+                drawImage("case", i*43, j*38);
+            } else if(map[i][j]=='P'){
+                drawImage("case", i*43, j*38);
+                drawImage("player1_1", i*43, j*38);
+            } else if(map[i][j]=='Q'){
+                drawImage("case", i*43, j*38);
+                drawImage("player2", i*43, j*38);
+            }
+        }
+    }
+    //drawImage("player2", 9*43, 11*38);
+}
+
+function checkKey(e) {
+    e = e || window.event;
+
+    if (e.keyCode == '38') {
+        // up arrow
+        if(P1y>1 && map[P1x][P1y-1]==' '){
+            map[P1x][P1y]=' ';
+            P1y=P1y-1;
+            map[P1x][P1y]='P';
+        }
+    }
+    else if (e.keyCode == '40') {
+        // down arrow
+        if(P1y<width-2 && map[P1x][P1y+1]==' '){
+            map[P1x][P1y]=' ';
+            P1y=P1y+1;
+            map[P1x][P1y]='P';
+        }
+    }
+    else if (e.keyCode == '37') {
+        // left arrow
+        if(P1x>1 && map[P1x-1][P1y]==' '){
+            map[P1x][P1y]=' ';
+            P1x=P1x-1;
+            map[P1x][P1y]='P';
+        }
+    }
+    else if (e.keyCode == '39') {
+        // right arrow
+        if(P1x<height-2 && map[P1x+1][P1y]==' '){
+            map[P1x][P1y]=' ';
+            P1x=P1x+1;
+            map[P1x][P1y]='P';
+            
         }
     }
 
+    drawMap();
 }
 
+
 window.addEventListener("load", function (event) {
-    var map = randomMap(11,13);
-    drawMap(map);
+    document.onkeydown = checkKey;
+    randomMap(20);
+    drawMap();
+    //drawPlayer(map)
 });
