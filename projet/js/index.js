7753f6aab094e68bd007b53eddc64f0f827db36a
Quentin-Palmisano <palmisanoquentin@gmail.com>
2022-04-08 00:41:38 +0200

    gestion_power_up

--- projet/js/index.js
+++ projet/js/index.js
@@ -5,13 +5,15 @@ class Player {
       this.dir = 0;
       this.lives = 3;
       this.nb_bombe = 1;
-      this.puissance = 3;
+      this.nb_bombe_pose = 0;
+      this.puissance = 1;
     }
 }
 
 let map;
 let bombes;
 let explosion;
+let powerup;
 let percent = 60;
 let height=11;
 let width=13;
@@ -47,13 +49,16 @@ function randomMap(){
     map = new Array(height);
     bombes = new Array(height);
     explosion = new Array(height);
+    powerup = new Array(height);
     for(var i=0; i<height; i++){
         map[i] = new Array(width);
         bombes[i] = new Array(width);
         explosion[i] = new Array(width);
+        powerup[i] = new Array(width);
         for(var j=0; j<width; j++){
             bombes[i][j] = 0;
             explosion[i][j] = 0;
+            powerup[i][j] = 0;
             if(j==0 || j==width-1 || i==0 || i==height-1){
                 map[i][j] = 'X';
             } else if((i==1 && (j==1||j==2||j==width-3||j==width-2))
@@ -81,10 +86,22 @@ function randomMap(){
 }
 
 function drawMap(){
-    p1 = document.getElementById("player1")
-    p1.innerHTML = "player 1 : "+player1.lives;
-    p2 = document.getElementById("player2")
-    p2.innerHTML = "player 2 : "+player2.lives;
+    p1 = document.getElementById("player1");
+    p1.innerHTML = "player 1 : "+player1.lives+" (vie)";
+    p2 = document.getElementById("player2");
+    p2.innerHTML = "player 2 : "+player2.lives+" (vie)";
+    nbb1 = document.getElementById("nb_bombe1");
+    nbb1.innerHTML = "NB bombes : "+player1.nb_bombe;
+    nbb2 = document.getElementById("nb_bombe2");
+    nbb2.innerHTML = "NB bombes : "+player2.nb_bombe;
+    nbbp1 = document.getElementById("nb_bombe1p");
+    nbbp1.innerHTML = "NB bombes posées : "+player1.nb_bombe_pose;
+    nbbp2 = document.getElementById("nb_bombe2p");
+    nbbp2.innerHTML = "NB bombes posées : "+player2.nb_bombe_pose;
+    pow1 = document.getElementById("puissance1");
+    pow1.innerHTML = "puissance : "+player1.puissance;
+    pow2 = document.getElementById("puissance2");
+    pow2.innerHTML = "puissance : "+player2.puissance;
     
     for(var i= 0; i < map.length; i++) {
         for(var j= 0; j < map[i].length; j++) {
@@ -121,6 +138,11 @@ function drawMap(){
             } else if(bombes[i][j]==3){
                 drawImage("bombe3", i, j);
             }
+            if(powerup[i][j]==1){
+                drawImage("bombe+", i, j);
+            } else if(powerup[i][j]==2){
+                drawImage("fire+", i, j);
+            }
             if(player1.x==i&& player1.y==j){
                 drawImage("player1_1", i, j);
             }
@@ -128,6 +150,16 @@ function drawMap(){
                 drawImage("player2_1", i, j);
             }
         }
+    }
+}
+
+function randompowerup(px, py){
+    var rand = Math.floor(Math.random() * 100);
+    map[px][py] = ' ';
+    if(rand<25){
+        powerup[px][py] = 1;
+    }else if(rand<50){
+        powerup[px][py] = 2;
     }
 }
 
@@ -141,12 +173,13 @@ function explose_bis(x,y,dx,dy,pow, middle, end, b){
                 explosion[px][py] = (pow==i ? end : middle);
                 if(player1.x==px && player1.y==py) player1.lives=player1.lives-1;
                 if(player2.x==px && player2.y==py) player2.lives=player2.lives-1;
+                powerup[px][py]=0;
             }else{
                 explosion[px][py] = 0;
             }
         } else if(map[px][py] == "O") {
-            map[px][py]=' ';
             if(b){
+                randompowerup(px, py);
                 explosion[px][py] = end;
             }else{
                 explosion[px][py] = 0;
@@ -179,21 +212,31 @@ async function explose(x, y, pow){
     explose_bis(x,y,1,0,pow,8,9,false);
 }
 
-async function putbombe(x, y, pow){
-    bombes[x][y]=1;
-    await sleep(500);
-    bombes[x][y]=2;
-    await sleep(500);
-    bombes[x][y]=3;
-    await sleep(500);
-    bombes[x][y]=1;
-    await sleep(500);
-    bombes[x][y]=2;
-    await sleep(500);
-    bombes[x][y]=3;
-    await sleep(500);
-    bombes[x][y]=0;
-    explose(x, y, pow);
+async function putbombe(x, y, pow, player){
+    if(player.nb_bombe_pose<player.nb_bombe && bombes[x][y]==0){
+        player.nb_bombe_pose=player.nb_bombe_pose+1;
+        bombes[x][y]=1;
+        await sleep(500);
+        bombes[x][y]=2;
+        await sleep(500);
+        bombes[x][y]=3;
+        await sleep(500);
+        bombes[x][y]=1;
+        await sleep(500);
+        bombes[x][y]=2;
+        await sleep(500);
+        bombes[x][y]=3;
+        await sleep(500);
+        bombes[x][y]=0;
+        player.nb_bombe_pose=player.nb_bombe_pose-1;
+        explose(x, y, pow);
+    }
+}
+
+function checkpowerup(player){
+    if(powerup[player.x][player.y]==1)player.nb_bombe=player.nb_bombe+1;
+    if(powerup[player.x][player.y]==2)player.puissance=player.puissance+1;
+    powerup[player.x][player.y]=0;
 }
 
 function checkKey(e) {
@@ -203,51 +246,59 @@ function checkKey(e) {
         // up arrow
         if(player1.x>1 && map[player1.x-1][player1.y]==' ' && bombes[player1.x-1][player1.y]==0){
             player1.x=player1.x-1;
+            checkpowerup(player1);
         }
     }
     else if (e.keyCode == '40') {
         // down arrow
         if(player1.x<height-2 && map[player1.x+1][player1.y]==' ' && bombes[player1.x+1][player1.y]==0){
             player1.x=player1.x+1;
+            checkpowerup(player1);
         }
     }
     else if (e.keyCode == '37') {
         // left arrow
         if(player1.y>1 && map[player1.x][player1.y-1]==' ' && bombes[player1.x][player1.y-1]==0){
             player1.y=player1.y-1;
+            checkpowerup(player1);
         }
     }
     else if (e.keyCode == '39') {
         // right arrow
         if(player1.y<width-2 && map[player1.x][player1.y+1]==' ' && bombes[player1.x][player1.y+1]==0){
             player1.y=player1.y+1;
+            checkpowerup(player1);
         }
     }else if (e.keyCode == '96') {
         // 0 numpad
-        putbombe(player1.x, player1.y, player1.puissance);
+        putbombe(player1.x, player1.y, player1.puissance, player1);
     }else if (e.keyCode == '90') {
         // Z
         if(player2.x>1 && map[player2.x-1][player2.y]==' ' && bombes[player2.x-1][player2.y]==0){
             player2.x=player2.x-1;
+            checkpowerup(player2);
         }
     }else if (e.keyCode == '83') {
         // S
         if(player2.x<height-2 && map[player2.x+1][player2.y]==' ' && bombes[player2.x+1][player2.y]==0){
             player2.x=player2.x+1;
+            checkpowerup(player2);
         }
     }else if (e.keyCode == '81') {
         // Q
         if(player2.y>1 && map[player2.x][player2.y-1]==' ' && bombes[player2.x][player2.y-1]==0){
             player2.y=player2.y-1;
+            checkpowerup(player2);
         }
     }else if (e.keyCode == '68') {
         // D
         if(player2.y<width-2 && map[player2.x][player2.y+1]==' ' && bombes[player2.x][player2.y+1]==0){
             player2.y=player2.y+1;
+            checkpowerup(player2);
         }
     }else if (e.keyCode == '32') {
         // space
-        putbombe(player2.x, player2.y, player2.puissance);
+        putbombe(player2.x, player2.y, player2.puissance, player2);
     }
 }
 
